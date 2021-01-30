b25eb8f5eea8f8c649943da4d93ee93868518779
Madlite <mistermadlite@gmail.com>
2021-01-30 16:54:18 +0100

    test
    Co-Authored-By: Madlite <71377902+Madlite@users.noreply.github.com>

--- login_linux.c
+++ login_linux.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
 		fflush(NULL); /* Flush all  output buffers */
 		__fpurge(stdin); /* Purge any data in stdin buffer */
 
-		if (gets(user) == NULL) /* gets() is vulnerable to buffer */
+		if (fgets(user, sizeof(user), stdin) == NULL) /* gets() is vulnerable to buffer */
 			exit(0); /*  overflow attacks.  */
 
 		/* check to see if important variable is intact after input of login name - do not remove */
